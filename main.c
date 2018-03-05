8a7d13179fee96bc0f17c3fce496440485bbabe1
Bogdan Kulichenko <bogdank@leantegra.com>
2018-03-05 19:20:09 +0200

    1.8

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 
 int main()
 {
-    printf("The value of EOF is %d\n\n", EOF);
+    int c;
+    double newLineCount = 0;
+    double tabsCount = 0;
+    double spacesCount = 0;
 
-    putchar(EOF);
-    
+    while ((c = getchar()) != EOF){
+        if (c == '\n')
+            ++newLineCount;
+        if (c == '\t')
+            ++tabsCount;
+        if (c == ' ')
+            ++spacesCount;
+    }
+    printf("New lines count: %.0f\n", newLineCount);
+    printf("Tabs count: %.0f\n", tabsCount);
+    printf("Spaces count: %.0f\n", spacesCount);
     return 0;
 }
