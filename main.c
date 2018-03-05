e9304635e91a5d534abb701a97702deaaa16cd02
Bogdan Kulichenko <bogdank@leantegra.com>
2018-03-05 19:43:20 +0200

    1.9

--- main.c
+++ main.c
@@ -2,21 +2,19 @@
 
 int main()
 {
-    int c;
-    double newLineCount = 0;
-    double tabsCount = 0;
-    double spacesCount = 0;
+    int c, flag;
+    
+    flag = 0;
 
     while ((c = getchar()) != EOF){
-        if (c == '\n')
-            ++newLineCount;
-        if (c == '\t')
-            ++tabsCount;
-        if (c == ' ')
-            ++spacesCount;
+        if (c != ' '){
+            putchar(c);
+            flag = 0;
+        }
+        else if (flag == 0){
+            putchar(c);
+            flag = 1;
+        }
     }
-    printf("New lines count: %.0f\n", newLineCount);
-    printf("Tabs count: %.0f\n", tabsCount);
-    printf("Spaces count: %.0f\n", spacesCount);
     return 0;
 }
