0da09f0b1244f67cc967353eeff0241f9639be9c
Bogdan Kulichenko <bogdank@leantegra.com>
2018-03-05 18:20:35 +0200

    CHAPTER_1: exercise 1.5

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
-    printf("Hello, World!\n");
+    int fahr;
+
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
+        printf("%3d\t%4.1f", fahr, (5.0/9.0) * (fahr - 32));
+    }
+
     return 0;
 }
