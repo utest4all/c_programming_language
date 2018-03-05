7c536988af6fc508e1366424ed27bad4bc8b19f7
Bogdan Kulichenko <bogdank@leantegra.com>
2018-03-05 18:56:04 +0200

    CHAPTER_1: 1.6

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
+int main()
+{
+    int c;
 
-int main() {
-    int fahr;
-
-    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
-        printf("%3d\t%4.1f", fahr, (5.0/9.0) * (fahr - 32));
-    }
+    c = getchar() != EOF;
+    if (c == 1)
+        printf("1");
+    else
+        printf("0");
 
     return 0;
 }
