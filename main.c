7055560ef70dd5165a04a9bf2fa5ad764862099d
utest4all <example@example.com>
2018-03-06 19:42:40 +0200

    1.13

--- main.c
+++ main.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 
-#define IN   1 /*inside word*/
-#define OUT  0 /*outside word*/
+#define MAXLEN 10 /*max word len*/
 
 int main() {
-    int c, state;
+    int i, c, nextWordCounter;
 
-    state = OUT;
+    int wordLenght[MAXLEN];
 
-    while((c = getchar()) != EOF){
-        if (c != ' ' && c != '\n' && '\t'){
-            state = IN;
-            putchar(c);
-        }
-        else if ( state == IN ) {
-            state = OUT;
-            putchar('\n');
-        }
+    nextWordCounter = 0;
+
+    for (i = 0; i < MAXLEN; ++i)
+    {
+        wordLenght[i] = 0;
     }
+
+    while ((c = getchar()) != EOF )
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            wordLenght[nextWordCounter] = wordLenght[nextWordCounter] + 1;
+            nextWordCounter = 0;
+        }
+        else
+        {
+            ++nextWordCounter;
+        }
+
+    }
+
+    for (i = 0; i < MAXLEN; ++i)
+    {
+        printf("word count %d: %d\n", i, wordLenght[i]);
+    }
+
+    printf("Hystogramm goes here\n");
+
+    for (i = 0; i < MAXLEN; ++i)
+    {
+        printf("%d > ", i);
+        for (int j = 0; j <= wordLenght[i]; ++j)
+        {
+            printf("|");
+        }
+        printf("\n");
+    }
+
+
 }
