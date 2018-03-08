84b7cb4718d0f28cd0f7d1a18c0760f7881e0535
utest4all <example@example.com>
2018-03-08 12:08:01 +0200

    1.15

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
-#include <string.h>
+#define LOWER 0;
+#define UPPER 200;
+#define STEP 20;
 
-int main() {
+int convertFahrToCelsius(int fahr);
 
 
+int main()
+{
+
+    int fahr, step, upper;
+
+    fahr = LOWER;
+    step = STEP;
+    upper = UPPER;
+
+    while (fahr <= upper)
+    {
+        printf("%d %d\n", fahr, convertFahrToCelsius(fahr));
+        fahr = fahr + step;
+    }
+
+    return 0;
+
+}
+
+
+int convertFahrToCelsius(int fahr)
+{
+    int celsius;
+
+    celsius = 5 * (fahr - 32) / 9;
+
+    return celsius;
 }
